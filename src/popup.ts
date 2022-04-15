92d8e92a15d4c4674532cf82e23829439c9a8658
Nils Alex <nils@famalex.de>
2022-04-15 22:52:54 +0200

    Introduce typescript setup

---
+++ src/popup.ts
@@ -0,0 +1,42 @@
+window.onload = () => document.getElementById('totpKey')?.focus();
+
+const displayResult = (result: any) => {
+  const node = document.getElementById('code');
+
+  if (node == null) {
+    return;
+  }
+
+  if (result.code) {
+    node.innerText = result.code;
+
+    const selection = window.getSelection();
+    const range = document.createRange();
+
+    selection?.removeAllRanges();
+    range.selectNodeContents(node);
+    selection?.addRange(range);
+  } else {
+    node.innerText = JSON.stringify(result);
+  }
+};
+
+interface TotpFormElements extends HTMLFormControlsCollection {
+  totpKey: HTMLInputElement
+}
+
+document.getElementById('getTotp')?.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const key = ((e.target as HTMLFormElement).elements as TotpFormElements).totpKey.value;
+
+  const displayElement = document.getElementById('code');
+
+  if (displayElement == null) {
+    return;
+  }
+
+  displayElement.innerText = 'getting OTP ... (touch YubiKey?)';
+
+  chrome.runtime.sendMessage(key, displayResult);
+});
